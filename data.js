91a05150c2d43e0298c4dafc8a74f1bb880b95ba
Ponsanjai <139705530+Ponsanjai@users.noreply.github.com>
2024-09-06 10:44:23 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,27 @@
+let movies = [
+    {
+        name: 'falcon and the winter soldier',
+        des: '"The Falcon and the Winter Soldier" is a Marvel Cinematic Universe series, not a movie. It follows Sam Wilson (Falcon) and Bucky Barnes (Winter Soldier) as they team up to confront new threats after the events of Avengers: Endgame. The series delves into themes of legacy, identity, and the challenges of stepping into Captain America shoes while facing complex global challenges.',
+        image: 'images/slider 2.PNG'
+    },
+    {
+        name: 'loki',
+        des: '"Loki" is a Marvel Cinematic Universe series that follows the God of Mischief, Loki, after he escapes with the Tesseract during the events of "Avengers: Endgame." The show explores Loki journey through alternate timelines, his encounters with the Time Variance Authority (TVA), and his discovery of a greater cosmic threat. The series blends action, humor, and complex character development, with Tom Hiddleston reprising his role as the iconic trickster.',
+        image: 'images/slider 1.PNG'
+    },
+    {
+        name: 'wanda vision',
+        des: '"WandaVision" is a Marvel Studios series blending classic television sitcoms with the Marvel Cinematic Universe. It follows Wanda Maximoff (Scarlet Witch) and Vision, living an idyllic suburban life, as they begin to suspect that everything is not as it seems. The show explores themes of grief, love, and reality, while unraveling a complex mystery through its unique episodic format.',
+        image: 'images/slider 3.PNG'
+    },
+    {
+        name: 'raya and the last dragon',
+        des: '"Raya and the Last Dragon" is a Disney animated film set in the mystical land of Kumandra, where a lone warrior, Raya, embarks on a quest to find the last dragon, Sisu, to save her divided world from an ancient evil. The movie explores themes of trust, unity, and the power of teamwork. With stunning visuals and a rich cultural backdrop, it delivers a heartwarming adventure filled with action and fantasy.',
+        image: 'images/slider 4.PNG'
+    },
+    {
+        name: 'luca',
+        des: 'Luca is a 2021 animated film by Pixar that tells the story of a young sea monster named Luca, who discovers a world above the water on the Italian Riviera. As he experiences life as a human with his new friend Alberto, they embark on an adventure filled with friendship, self-discovery, and the challenges of hiding their true identities. The film beautifully explores themes of acceptance, belonging, and the courage to embrace one trueself',
+        image: 'images/slider 5.PNG'
+    }
+];
